366e14c9d0290e35df5d122e575a7d6567e1fe38
superclash89 <superclash89@gmail.com>
2019-11-18 10:45:21 -0500

    Update Maps.java

--- src/ActualScenario/Maps.java
+++ src/ActualScenario/Maps.java
@@ -7,7 +7,7 @@ import TestScenario.testMap.testTile2;
 public class Maps {
 	public static class OuterField extends BaseMechanics.Map {
 		{
-		this.grid = new Tile[18][9];
+		this.grid = new Tile[16][12];
 		for(int x = 0; x < grid.length; x++) {
 			for(int y = 0; y < grid[x].length; y++) {
 				grid[x][y] = new Tiles.BasicField();
