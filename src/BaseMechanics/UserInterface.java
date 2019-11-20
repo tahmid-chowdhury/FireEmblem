a7439ce42337f2f344e597e31ffa5762b2937074
superclash89 <superclash89@gmail.com>
2019-11-20 10:08:18 -0500

    Update UserInterface.java

--- src/BaseMechanics/UserInterface.java
+++ src/BaseMechanics/UserInterface.java
@@ -1,7 +1,19 @@
 package BaseMechanics;
 
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
 public class UserInterface {
-	public void paint(){
-		
+	
+	ArrayList<Element> elements;
+	
+	public void paint(Graphics2D g){
+		for(Element x: elements){
+			x.paint(g);		
+		}
+	}
+	
+	public static abstract class Element {
+		public abstract void paint(Graphics2D g);
 	}
 }
