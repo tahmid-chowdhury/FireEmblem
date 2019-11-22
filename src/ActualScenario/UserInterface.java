c539da83cba24de2a48d532456d1a364469d4be4
superclash89 <superclash89@gmail.com>
2019-11-22 10:01:52 -0500

    Update UserInterface.java

--- src/ActualScenario/UserInterface.java
+++ src/ActualScenario/UserInterface.java
@@ -2,7 +2,11 @@ package ActualScenario;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import BaseMechanics.AllTogether;
@@ -29,16 +33,22 @@ public class UserInterface extends BaseMechanics.UserInterface {
 	
 	public static class border extends BaseMechanics.UserInterface.Element {
 		
+		ArrayList<BufferedImage> sprites;
 		
-		
+		public border(){
+			sprites = new ArrayList<BufferedImage>(0);
+			try{
+				sprites.add(ImageIO.read(new File("sprites/Gui/gui top.png")));
+
+			}catch(Exception e){
+				
+			}
+		}
+	
 		@Override
 		public void paint(Graphics2D g) {
-			g.setColor(Color.red);
-			g.drawString("Look mom I'm on a computer screen!", 69, 69);
-			g.fillRect((int)(xsize*0.05), (int)(ysize*0.65), (int)(xsize*0.9), (int)(ysize*0.25));
 			
-			g.setColor(Color.WHITE);
-			
+			g.fillRect((int)(xsize*0.05), (int)(ysize*0.65), (int)(xsize*0.9), (int)(ysize*0.25));						
 		}
 
 		@Override
