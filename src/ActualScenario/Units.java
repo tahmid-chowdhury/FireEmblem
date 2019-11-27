29af6efa4a23540a0ceb97bee5f9b75a47cfceec
superclash89 <superclash89@gmail.com>
2019-11-27 09:30:37 -0500

    ZS

--- src/ActualScenario/Units.java
+++ src/ActualScenario/Units.java
@@ -70,4 +70,54 @@ public class Units {
 					0, 0, sprites[1].getWidth(), sprites[1].getHeight(), null);
 		}
 	}
+	
+	public static class baseAssin extends BaseMechanics.Unit{
+		public baseAssin(){
+			this.sprites = new BufferedImage[2];
+			try{
+				sprites[0] = ImageIO.read(new File("sprites/characters/body/fassassinbody.png"));
+				sprites[1] = ImageIO.read(new File("sprites/characters/heads/fhead3.png"));
+				sprites[2] = ImageIO.read(new File("sprites/characters/heads/fhead3back.png"));
+
+			}catch(Exception e){
+				
+			}
+		}
+
+		public void paint(Graphics2D g, int x, int y, double scaleFactor, double xOffset, double yOffset, int xSize, int ySize){
+		/*	g.drawImage(sprites[2], 
+					//destination x co-ord 1
+					(int)(x*sprites[2].getWidth()*scaleFactor) + (int) (xOffset*(sprites[2].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 1
+					(int)(y*sprites[2].getHeight()*scaleFactor) + (int) (yOffset*(sprites[2].getHeight()*scaleFactor*ySize)), 
+					
+					//destination x co-ord 2
+					(int)(x*sprites[2].getWidth()*scaleFactor + sprites[2].getWidth()*scaleFactor) + (int) (xOffset*(sprites[2].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 2
+					(int)(y*sprites[2].getHeight()*scaleFactor + sprites[2].getHeight()*scaleFactor) + (int) (yOffset*(sprites[2].getHeight()*scaleFactor*ySize)),
+					
+					
+					0, 0, sprites[2].getWidth(), sprites[2].getHeight(), null);
+		*/	
+			super.paint(g, x, y, scaleFactor, xOffset, yOffset, xSize, ySize);
+			
+			g.drawImage(sprites[1], 
+					//destination x co-ord 1
+					(int)(x*sprites[1].getWidth()*scaleFactor) + (int) (xOffset*(sprites[1].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 1
+					(int)(y*sprites[1].getHeight()*scaleFactor) + (int) (yOffset*(sprites[1].getHeight()*scaleFactor*ySize)), 
+					
+					//destination x co-ord 2
+					(int)(x*sprites[1].getWidth()*scaleFactor + sprites[1].getWidth()*scaleFactor) + (int) (xOffset*(sprites[1].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 2
+					(int)(y*sprites[1].getHeight()*scaleFactor + sprites[1].getHeight()*scaleFactor) + (int) (yOffset*(sprites[1].getHeight()*scaleFactor*ySize)),
+					
+					
+					0, 0, sprites[1].getWidth(), sprites[1].getHeight(), null);
+		}
+	}
 }
