0a8911ca499a786df4f2dd8c23b23d11c85e26bf
superclash89 <superclash89@gmail.com>
2019-12-05 10:50:26 -0500

    haha snow

--- src/ActualScenario/Maps.java
+++ src/ActualScenario/Maps.java
@@ -18,12 +18,7 @@ public class Maps {
 					}else if(x == 1){
 						grid[x][y] = new Tiles.LeftPathField();
 					}
-				}
-				
-				if( x > 8 && x < 12){
-						grid[x][y] = new Tiles.Path();
-					}
-				
+				}	
 				if(y == 2){
 					if(x!=2&&x!=1){
 					grid[x][y] = new Tiles.BottomPathField();
@@ -32,7 +27,11 @@ public class Maps {
 						if(x!=2&&x!=1){
 						grid[x][y] = new Tiles.TopPathField();
 						}
-					}
+				}
+				if( x == 0 || x == grid.length-1 || y == 0 || y == grid[x].length-1){
+					grid[x][y] = new Tiles.Snow();
+				}
+			
 			}
 		}
 		grid[4][4].occupyingUnit = new Units.baseRanger();
