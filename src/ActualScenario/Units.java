5e3a4997532db96c8691be064b19b0c12ef05631
RSpyno <57759744+RSpyno@users.noreply.github.com>
2019-12-13 09:24:58 -0500

    EDGE

--- src/ActualScenario/Units.java
+++ src/ActualScenario/Units.java
@@ -197,6 +197,59 @@ public class Units {
 		}
 	}
 	
+	public static class selfEntitled extends BaseMechanics.Unit{
+		public selfEntitled(){
+			this.sprites = new BufferedImage[3];
+			this.speed = 2;
+			this.type = Unit.Type.MELEE;
+			try{
+				sprites[0] = ImageIO.read(new File("sprites/characters/body/mbody5.png"));
+				sprites[1] = ImageIO.read(new File("sprites/characters/heads/mhead5.png"));
+				sprites[2] = ImageIO.read(new File("sprites/characters/heads/mhead5back.png"));
+			}catch(Exception e){
+			
+			}
+		}
+	
+		public void paint(Graphics2D g, int x, int y, double scaleFactor, double xOffset, double yOffset, int xSize, int ySize){
+			
+			g.drawImage(sprites[2], 
+					//destination x co-ord 1
+					(int)(x*sprites[2].getWidth()*scaleFactor) + (int) (xOffset*(sprites[2].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 1
+					(int)(y*sprites[2].getHeight()*scaleFactor) + (int) (yOffset*(sprites[2].getHeight()*scaleFactor*ySize)), 
+					
+					//destination x co-ord 2
+					(int)(x*sprites[2].getWidth()*scaleFactor + sprites[2].getWidth()*scaleFactor) + (int) (xOffset*(sprites[2].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 2
+					(int)(y*sprites[2].getHeight()*scaleFactor + sprites[2].getHeight()*scaleFactor) + (int) (yOffset*(sprites[2].getHeight()*scaleFactor*ySize)),
+					
+					
+					0, 0, sprites[2].getWidth(), sprites[2].getHeight(), null);
+			
+			super.paint(g, x, y, scaleFactor, xOffset, yOffset, xSize, ySize);
+			
+			g.drawImage(sprites[1], 
+					//destination x co-ord 1
+					(int)(x*sprites[1].getWidth()*scaleFactor) + (int) (xOffset*(sprites[1].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 1
+					(int)(y*sprites[1].getHeight()*scaleFactor) + (int) (yOffset*(sprites[1].getHeight()*scaleFactor*ySize)), 
+					
+					//destination x co-ord 2
+					(int)(x*sprites[1].getWidth()*scaleFactor + sprites[1].getWidth()*scaleFactor) + (int) (xOffset*(sprites[1].getWidth()*scaleFactor*xSize)), 
+					
+					//destination y co-ord 2
+					(int)(y*sprites[1].getHeight()*scaleFactor + sprites[1].getHeight()*scaleFactor) + (int) (yOffset*(sprites[1].getHeight()*scaleFactor*ySize)),
+					
+					
+					0, 0, sprites[1].getWidth(), sprites[1].getHeight(), null);
+		}
+		
+	}
+	
 	public static class DwayneJohnson extends BaseMechanics.Unit{
 		public DwayneJohnson(){
 			this.sprites = new BufferedImage[1];
