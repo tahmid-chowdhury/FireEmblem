dc87ca8cd1be1506aebe07f2dc9e3b1acc88c084
superclash89 <superclash89@gmail.com>
2019-12-19 10:22:27 -0500

    added attack view

--- src/BaseMechanics/Map.java
+++ src/BaseMechanics/Map.java
@@ -41,7 +41,26 @@ public abstract class Map {
 		}else{
 			return false;
 		}
-	}	
+	}
+	 
+	public boolean checkAttack(int x1, int y1, int x2, int y2){
+		if(x1<0||x2<0||y1<0||y2<0||x1>=grid.length||x2>=grid.length||y1>=grid[x1].length||y2>=grid[x2].length){
+			return false;
+		}
+		else if(
+				grid[x1][y1].occupyingUnit!=null&&
+				grid[x2][y2].occupyingUnit!=null&&
+				grid[x1][y1].occupyingUnit.team!=grid[x2][y2].occupyingUnit.team&&
+				grid[x2][y2].passable&&
+				(Math.abs(grid[x2][y2].zAxis-grid[x1][y1].zAxis)==1||Math.abs(grid[x2][y2].zAxis-grid[x1][y1].zAxis)==0)&&
+				
+				Math.sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))) <= grid[x1][y1].occupyingUnit.speed
+				){
+			return true;
+		}else{
+			return false;
+		}
+	}
 	
 	/*private static class node{
 		int x; int y; int f;
