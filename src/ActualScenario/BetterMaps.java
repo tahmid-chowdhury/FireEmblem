4b9ce639b019bbd83843890cd2850c1a127f22b2
Tahmid Chowdhury <tahmid1203@hotmail.com>
2020-01-10 08:55:07 -0500

    Implemented the entirety of Pacman

--- src/ActualScenario/BetterMaps.java
+++ src/ActualScenario/BetterMaps.java
@@ -348,81 +348,249 @@ public class BetterMaps {
 						else if (y >= 18 && y <= 19) {
 							grid[x][y] = new Tiles.PacmanMid();
 						}
-						else if (y == 13 || y == 19) {
+						else if (y == 14 || y == 20) {
 							grid[x][y] = new Tiles.PacmanBottomRight();
 						}
 					}
 					else if (x == 3) {
-						if (y >= 8 && y <= 11) {
-							
+						if (y == 2 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 5 || y == 8) {
+							grid[x][y] = new Tiles.PacmanBlock();
+						}
+						else if (y == 23 || y == 26) {
+							grid[x][y] = new Tiles.PacmanLeft();
 						}
 					}
 					else if (x == 4) {
-						if (y >= 8 && y <= 9) {
-							
+						if (y == 2 || y == 23 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 26) {
+							grid[x][y] = new Tiles.PacmanRight();
 						}
 					}
 					else if (x == 5) {
-						if (y >= 8 && y <= 10) {
-							
+						if (y == 2 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 5) {
+							grid[x][y] = new Tiles.PacmanBlock();
+						}
+						else if (y == 9) {
+							grid[x][y] = new Tiles.PacmanTop();
+						}
+						else if (y == 10) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 11) {
+							grid[x][y] = new Tiles.PacmanBottomLeft();
+						}
+						else if (y == 14 || y == 17 || y == 20) {
+							grid[x][y] = new Tiles.PacmanLeft();
+						}
+						else if (y == 23) {
+							grid[x][y] = new Tiles.PacmanRight();
 						}
 					}
 					else if (x == 6) {
-						if (y >= 7 && y <= 10) {
-							
+						if (y == 2 || y == 20 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 11) {
+							grid[x][y] = new Tiles.PacmanRight();
+						}
+						else if (y == 14) {
+							grid[x][y] = new Tiles.PacmanTopRight();
+						}
+						else if (y == 15 || y == 16) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 17) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
 						}
 					}
 					else if (x == 7) {
-						if (y >= 7 && y <= 10) {
-							
+						if (y == 2) {
+							grid[x][y] = new Tiles.PacmanTopRight();
+						}
+						else if (y >= 3 && y <= 7) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 8) {
+							grid[x][y] = new Tiles.PacmanBottomLeft();
+						}
+						else if (y == 20) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 27) {
+							grid[x][y] = new Tiles.PacmanTopLeft();
+						}
+						else if (y == 28) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 29) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
 						}
 					}
 					else if (x == 8) {
-						if (y >= 6 && y <= 21) {
-							
+						if (y == 2) {
+							grid[x][y] = new Tiles.PacmanTopLeft();
+						}
+						else if (y >= 3 && y <= 7) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 8) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
+						}
+						else if (y == 20) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 27) {
+							grid[x][y] = new Tiles.PacmanTopRight();
+						}
+						else if (y == 28) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 29) {
+							grid[x][y] = new Tiles.PacmanBottomLeft();
 						}
 					}
 					else if (x == 9) {
-						if (y >= 6 && y <= 13) {
-							
+						if (y == 2 || y == 20 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 11) {
+							grid[x][y] = new Tiles.PacmanLeft();
+						}
+						else if (y == 14) {
+							grid[x][y] = new Tiles.PacmanTopLeft();
+						}
+						else if (y == 15 || y == 16) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 17) {
+							grid[x][y] = new Tiles.PacmanBottomLeft();
 						}
 					}
 					else if (x == 10) {
-						if (y >= 0 && y <= 8) {
-							
+						if (y == 2 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 5) {
+							grid[x][y] = new Tiles.PacmanBlock();
+						}
+						else if (y == 9) {
+							grid[x][y] = new Tiles.PacmanTop();
+						}
+						else if (y == 10) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 11) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
+						}
+						else if (y == 14 || y == 17 || y == 20) {
+							grid[x][y] = new Tiles.PacmanRight();
+						}
+						else if (y == 23) {
+							grid[x][y] = new Tiles.PacmanLeft();
 						}
 					}
 					else if (x == 11) {
-						if (y >= 0 && y <= 6) {
-							
+						if (y == 2 || y == 23 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 26) {
+							grid[x][y] = new Tiles.PacmanLeft();
 						}
 					}
 					else if (x == 12) {
-						if (y >= 19 && y <= 21) {
-							
+						if (y == 2 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
+						}
+						else if (y == 5 || y == 8) {
+							grid[x][y] = new Tiles.PacmanBlock();
+						}
+						else if (y == 23 || y == 26) {
+							grid[x][y] = new Tiles.PacmanRight();
 						}
 					}
 					else if (x == 13) {
-						if (y >= 20 && y <= 21) {
-							
+						if (y == 2 || y == 29) {
+							grid[x][y] = new Tiles.PacmanMidSide();
 						}
-					else if (x == 14) {
-						if (y >= 20 && y <= 21) {
-							
+						else if (y == 11 || y == 17) {
+							grid[x][y] = new Tiles.PacmanTopLeft();
+						}
+						else if (y >= 12 && y <= 13) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y >= 18 && y <= 19) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 14 || y == 20) {
+							grid[x][y] = new Tiles.PacmanBottomLeft();
 						}
 					}
-					else if (x == 15) {
-						if (y >= 20 && y <= 22) {
-							
+					else if (x == 14) {
+						if (y == 2) {
+							grid[x][y] = new Tiles.PacmanTopRight();
+						}
+						else if (y >= 3 && y <= 10) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 11) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
+						}
+						else if (y == 14) {
+							grid[x][y] = new Tiles.PacmanTopRight();
+						}
+						else if (y >= 15 && y <= 16) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 17) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
+						}
+						else if (y == 20) {
+							grid[x][y] = new Tiles.PacmanTopRight();
+						}
+						else if (y >= 21 && y <= 28) {
+							grid[x][y] = new Tiles.PacmanMid();
+						}
+						else if (y == 29) {
+							grid[x][y] = new Tiles.PacmanBottomRight();
 						}
 					}
 				}
 			}
-		
-			grid[0][0].occupyingUnit = new Units.baseRanger();
-			grid[0][0].occupyingUnit.team = Unit.Team.PLAYER1;
-				
-			}
+
+			grid[2][3].occupyingUnit = new Units.baseRanger();
+			grid[2][3].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER1;
+			grid[4][3].occupyingUnit = new Units.baseMage();
+			grid[4][3].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER1;
+			grid[6][3].occupyingUnit = new Units.foxFighter();
+			grid[6][3].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER1;
+			grid[9][3].occupyingUnit = new Units.basePriestess();
+			grid[9][3].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER1;
+			grid[11][3].occupyingUnit = new Units.mysticSwordsman();
+			grid[11][3].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER1;
+			grid[13][3].occupyingUnit = new Units.robot();
+			grid[13][3].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER1;
+
+			grid[2][28].occupyingUnit = new Units.mrDavis();
+			grid[2][28].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER2;
+			grid[4][28].occupyingUnit = new Units.baseAssin();
+			grid[4][28].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER2;
+			grid[6][28].occupyingUnit = new Units.selfEntitled();
+			grid[6][28].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER2;
+			grid[9][28].occupyingUnit = new Units.lonelyWanderer();
+			grid[9][28].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER2;
+			grid[11][28].occupyingUnit = new Units.baseSwordsman();
+			grid[11][28].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER2;
+			grid[13][28].occupyingUnit = new Units.femRanger();
+			grid[13][28].occupyingUnit.team = BaseMechanics.Unit.Team.PLAYER2;
+
 		}
 	}
 }
